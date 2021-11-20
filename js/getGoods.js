108ae1386826f6affd8ac908f454f5d33c62fe8b
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-11-21 01:51:57 +0300

    add localStorage

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,10 +1,27 @@
 const getGoods = () => {
-    fetch('https://willdberiess-default-rtdb.firebaseio.com/db.json')
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
+  const links = document.querySelectorAll('.navigation-link')
 
+  const getdata = () => {
+    fetch('https://willdberiess-default-rtdb.firebaseio.com/db.json')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+  }
+
+  links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault()
+      getdata()
+    })
+  })
+
+  localStorage.setItem('goods', JSON.stringify({ name: 'all' }))
+
+  const goods = JSON.parse(localStorage.getItem('goods'))
+
+
+localStorage.removeItem('goods')
 }
 
 getGoods()
