e0c8cd2286f8847c74533f25ffd634df4c315233
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-11-21 01:14:10 +0300

    add getGoods file

--- js/search.js
+++ js/search.js
@@ -2,7 +2,7 @@ const search = () => {
   const input = document.querySelector('.search-block > input')
   const searchBtn = document.querySelector('.search-block > button')
 
-  input.addEventListener('input', () => {
+  searchBtn.addEventListener('click', () => {
     console.log(input.value);
   })
 }
