c96a4a78df569b04d0a1435a0cddda932c9e22a3
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-11-22 00:37:30 +0300

    add ternar

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,13 +1,15 @@
 const getGoods = () => {
   const links = document.querySelectorAll('.navigation-link')
 
-  const getdata = () => {
+  const getdata = (value, category) => {
     fetch('https://willdberiess-default-rtdb.firebaseio.com/db.json')
       .then((res) => res.json())
       .then((data) => {
-        const array = data.filter((item) => {
-          return item.gender === 'Womens'
-        })
+        const array = data.filter((item) => 
+           item[category] === value
+           )
+
+           category ? console.log('есть') : console.log('нет');
 
         localStorage.setItem('goods', JSON.stringify(array))
       })
@@ -16,9 +18,10 @@ const getGoods = () => {
   links.forEach((link) => {
     link.addEventListener('click', (event) => {
       event.preventDefault()
+      const linkValue = link.textContent
+      const category = link.dataset.field
 
-      
-      getdata()
+      getdata(linkValue,category)
     })
   })
 }
