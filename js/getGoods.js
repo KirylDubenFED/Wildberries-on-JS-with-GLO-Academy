e954d6915f69c39845dbe2c2f700b922d16c7e9f
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-11-23 00:27:30 +0300

    add render goods

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,17 +1,24 @@
 const getGoods = () => {
   const links = document.querySelectorAll('.navigation-link')
+  const renderGoods = (goods) => {
+
+  }
 
   const getdata = (value, category) => {
     fetch('https://willdberiess-default-rtdb.firebaseio.com/db.json')
       .then((res) => res.json())
       .then((data) => {
-        const array = data.filter((item) => 
-           item[category] === value
-           )
-
-           category ? console.log('есть') : console.log('нет');
+        const array = category
+          ? data.filter((item) =>
+            item[category] === value) : data
 
         localStorage.setItem('goods', JSON.stringify(array))
+
+        if (window.location.pathname !== "/goods.html") {
+          window.location.href = '/goods.html'
+        } else {
+          renderGoods(array)
+        }
       })
   }
 
@@ -21,9 +28,14 @@ const getGoods = () => {
       const linkValue = link.textContent
       const category = link.dataset.field
 
-      getdata(linkValue,category)
+      getdata(linkValue, category)
     })
   })
+
+  if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
+    renderGoods(JSON.parse(localStorage.getItem('goods')))
+  }
+
 }
 
 getGoods()
