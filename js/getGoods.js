3be347832513d26e0d5eb088dc9fe3f4c9c9a660
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-11-23 01:21:15 +0300

    add filter

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,7 +1,32 @@
 const getGoods = () => {
   const links = document.querySelectorAll('.navigation-link')
   const renderGoods = (goods) => {
+    const goodsContainer = document.querySelector('.long-goods-list')
 
+goodsContainer.innerHTML = ""
+
+    goods.forEach(good => {
+      const goodBlock = document.createElement('div')
+
+      goodBlock.classList.add('col-lg-3')
+      goodBlock.classList.add('col-sm-6')
+
+      goodBlock.innerHTML = `
+   <div class="goods-card">
+						<span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+						<img src="db/${good.img}" alt="${good.name}" class="goods-image">
+						<h3 class="goods-title">${good.name}</h3>					
+						<p class="goods-description">${good.description}</p>
+						<button class="button goods-card-btn add-to-cart" data-id="007">
+							<span class="button-price">$${good.price}</span>
+						</button>
+					</div>
+   `
+goodsContainer.append(goodBlock)
+
+console.log(good);
+
+    })
   }
 
   const getdata = (value, category) => {
@@ -35,7 +60,6 @@ const getGoods = () => {
   if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
     renderGoods(JSON.parse(localStorage.getItem('goods')))
   }
-
 }
 
 getGoods()
